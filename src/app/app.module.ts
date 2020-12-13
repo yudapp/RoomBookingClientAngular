9fb36e15447f13b81cb8473e6dc15fca905d721f
yudApp <yappadoo@hotmail.com>
2020-12-13 15:46:38 +0000

    Add components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+const routes: Routes = [
+  {path: 'admin/users', component : UsersComponent},
+  {path: 'admin/rooms', component : RoomsComponent},
+  {path: '', component : CalendarComponent},
+  {path: '404', component : PagenotfoundComponent},
+  {path: '**', redirectTo: '/404'}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalendarComponent,
+    RoomsComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
